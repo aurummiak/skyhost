0f783b763b5f142c44164d61d6362599f0ef4a97
Anastasiae <aurummiak@gmail.com>
2025-03-06 21:28:30 +0300

    git

---
+++ js/script.js
@@ -0,0 +1,106 @@
+// jshint ignore:start 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.btn-testimonials--next',
+        prevEl: '.btn-testimonials--prev',
+    },
+
+});
+
+// jshint ignore:end 
+
+const header = document.querySelector('.header');
+const navbar = document.querySelector(".btn-burger");
+const menuTop = document.querySelector('.menu-top');
+const menuMiddle = document.querySelector('.menu-middle');
+const menuBottom = document.querySelector('.menu-bottom');
+const navItems = document.querySelectorAll('.nav__item');
+
+navbar.addEventListener('click', (event) => {
+
+    if (navbar.classList.contains('btn-burger--active')) {
+        resetNav();
+    }
+    else {
+        navbar.classList.add('btn-burger--active');
+        menuTop.classList.add('menu-top-click');
+        menuMiddle.classList.add('menu-middle-click');
+        menuBottom.classList.add('menu-bottom-click');
+        document.querySelector('.nav').classList.add('nav--active');
+
+        for (let i = 0; i < navItems.length; i++) {
+            navItems[i].classList.add('nav__item--active');
+
+            navItems[i].addEventListener('click', resetNav);
+        }
+        hideScroll();
+    }
+});
+
+const hideScroll = () => {
+    const scrollWidth = `${getScrollbarWidth()}px`;
+
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
+};
+
+const showScroll = () => {
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
+};
+
+const resetNav = () => {
+    navbar.classList.remove('btn-burger--active');
+    menuTop.classList.remove('menu-top-click');
+    menuMiddle.classList.remove('menu-middle-click');
+    menuBottom.classList.remove('menu-bottom-click');
+    document.querySelector('.nav').classList.remove('nav--active');
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('nav__item--active');
+    }
+    showScroll();
+};
+
+window.addEventListener('resize', resetNav);
+
+// Get scrollbar width
+const getScrollbarWidth = () => {
+
+    const outer = document.createElement('div');
+
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
+
+    document.body.appendChild(outer);
+    const scrollbarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
+
+    return scrollbarWidth;
+};
+
+let plansWrapper = document.querySelector('.plans-wrapper');
+
+plansWrapper.addEventListener("mouseover", () => {
+    document.querySelector('#secondItem').classList.remove('plans-wrapper__item--active');
+});
+
+plansWrapper.addEventListener("mouseout", () => {
+    document.querySelector('#secondItem').classList.add('plans-wrapper__item--active');
+});
+
+const footerLinks = document.querySelectorAll('.footer-item__title');
+
+footerLinks.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const tabId = e.target.getAttribute('data-link');
+        const currentItem = document.querySelector(tabId);
+        currentItem.classList.toggle('links-wrapper--active');
+    });
+});
